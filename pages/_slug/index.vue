9a328a36fc70bc856aa2bb9989bb8aa31be4b6f8
yori3 <yori3.otim2@gmail.com>
2022-01-08 18:12:19 +0900

    簡単なデモを作成

---
+++ pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="content"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://yori3.microcms.io/api/v1/news/${params.slug}`,
+      {
+        headers: { 'X-MICROCMS-API-KEY': '86d3f9d6-e86e-416a-84ab-7de805c462ae' }
+      }
+    )
+    return data
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+<div id="content">
+  <h1>お知らせ一覧</h1>
+  <div class="newsList">
+    <div class="newsList__item" v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        <h2>{{ content.title }}</h2>
+      </nuxt-link>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      'https://yori3.microcms.io/api/v1/news',
+      {
+        headers: { 'X-MICROCMS-API-KEY': '86d3f9d6-e86e-416a-84ab-7de805c462ae' }
+      }
+    )
+    return data
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+a{
+  color: inherit;
+}
+h1{
+  position: relative;
+  padding-left: 20px;
+  color: #333;
+  &::after{
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 10px;
+    height: 100%;
+    background-color: blue;
+    content: "";
+  }
+}
+
+.newsList{
+  &__item{
+    margin-bottom: 1.5em;
+    h2{
+      color: #666;
+    }
+  }
+}
+
+
+</style>
